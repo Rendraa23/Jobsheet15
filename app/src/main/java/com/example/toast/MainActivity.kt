0c18dc665c441e5edea4833f6aed2e3305e4bcd3
dapyaaa <alfiandavi48@gmail.com>
2024-01-15 09:20:56 +0700

    tosdlu

---
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.toast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       val tombol = findViewById<Button>(R.id.nama)
+        val tombol1 = findViewById<Button>(R.id.Kelas)
+        val tombol2 = findViewById<Button>(R.id.Nis)
+
+        tombol.setOnClickListener {
+            Toast.makeText(this,"Alfian Davi Aghna",Toast.LENGTH_SHORT).show()
+        }
+        tombol1.setOnClickListener {
+            Toast.makeText(this,"XI PPLG 1",Toast.LENGTH_SHORT).show()
+        }
+        tombol2.setOnClickListener {
+            Toast.makeText(this,"2233244",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
